ccc5240ea7bede043e9b9e85c565ba0252838ebf
haebiyve <126192566+haebiyve@users.noreply.github.com>
2023-04-24 20:01:05 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@
 #include "PM2_Drivers.h"
 
 
+// Knopf
 bool do_execute_main_task = false;
 bool do_reset_all_once = false;
 
-    //  Konstanten für DC Motor aus PM2_pes_board_example kopiert
+//  Konstanten für DC Motor aus PM2_pes_board_example kopiert
     const float max_voltage = 12.0f;               // define maximum voltage of battery packs, adjust this to 6.0f V if you only use one batterypack
     const float counts_per_turn = 20.0f * 78.125f; // define counts per turn at gearbox end: counts/turn * gearratio
     const float kn = 180.0f / 12.0f;               // define motor constant in RPM/V
@@ -18,19 +19,21 @@ bool do_reset_all_once = false;
     float Rest =0;
     float weg = 0;
     
-  // Konstanten für Stepper-Motor
+// Konstanten für Stepper-Motor
     const float winkelcorr= 0.14/180;
-    float Dutycicle=0.0f;
+    float Dutycicle=0.14f;
 
-DebounceIn user_button(PC_13);  //Blauerknopf
-DigitalOut user_led(LED1);        // LED Objekt auf STM Board. LED1 ist Connectet zu PA_5
-DigitalOut additional_led(PB_9); // Zusätzliches externes LED Objekt. PB_9 ist noch nicht fix.
-Servo servo(PB_12);                // Servo Objekt. PB_2 ist noch nicht fix.
-FastPWM pwm_M3(PA_10);            // DC Motor Objekt. Winkel kontrolliert durch Encoder. PA_10 noch nicht fix.
-EncoderCounter encoder_M3(PA_0, PA_1); // Encoder Objekt. PA_0 und PA_1 ist noch nicht fix.
-DigitalOut enable_motors(PB_15); // create DigitalOut object to enable dc motors
-PositionController positionController_M3(counts_per_turn * k_gear, kn / k_gear, max_voltage, pwm_M3, encoder_M3);     
-    //positionController_M3.setSpeedCntrlGain(kp * k_gear); 
+//Board Input
+
+    DebounceIn user_button(PC_13);  //Blauerknopf
+    DigitalOut user_led(LED1);        // LED Objekt auf STM Board. LED1 ist Connectet zu PA_5
+    DigitalOut additional_led(PB_9); // Zusätzliches externes LED Objekt. PB_9 ist noch nicht fix.
+    Servo servo(PB_12);                // Servo Objekt. PB_2 ist noch nicht fix.
+    FastPWM pwm_M3(PA_10);            // DC Motor Objekt. Winkel kontrolliert durch Encoder. PA_10 noch nicht fix.
+    EncoderCounter encoder_M3(PA_0, PA_1); // Encoder Objekt. PA_0 und PA_1 ist noch nicht fix.
+    DigitalOut enable_motors(PB_15); // create DigitalOut object to enable dc motors
+    PositionController positionController_M3(counts_per_turn * k_gear, kn / k_gear, max_voltage, pwm_M3, encoder_M3);//positionController_M3.setSpeedCntrlGain(kp * k_gear);    
+    
 
 void user_button_pressed_fcn();
 
@@ -51,37 +54,27 @@ void lift(float ang){
 
         while (Dutycicle <(ang*winkelcorr)) { //Robi Neutrale Stellung
         servo.setNormalisedAngle(Dutycicle);
-        Dutycicle +=0.0002f;
-        ThisThread::sleep_for(25ms);}
+        Dutycicle +=0.0003f;
+        ThisThread::sleep_for(10ms);}
 }
 
 void fall(float ang){
 
         while (Dutycicle >(ang*winkelcorr)) { //Robi Neutrale Stellung
         servo.setNormalisedAngle(Dutycicle);
-        Dutycicle -=0.0002f;
-        ThisThread::sleep_for(21ms);}
+        Dutycicle -=0.0003f;
+        ThisThread::sleep_for(10ms);}
 }
 
 int main() {
 
 user_button.fall(&user_button_pressed_fcn);
 
-    // Erstellte konstanten für Switch Zustände
-    const int arm_strecken_bis_flach = 1;
-    const int bis_zum_Hindernis_fahren = 2;
-    const int arm_heben_bis_hoehe_hindernis = 3;
-    const int fahren_bis_gelenk_ueber_hindernis = 4;
-    const int arm_strecken_bis_bodenkontakt = 5;
-    const int fahren_bis_hinderniskante = 6;
-    const int arm_heben_bis_flach = 7;
-    const int fahren_bis_ziel = 8;
-    const int arm_heben_in_ausgangsposition = 9;
-
-
-    int zustand = arm_strecken_bis_flach;
+    int zustand = 1;
     bool schleife = true;
-    servo.disable();
+    
+    servo.enable();
+    servo.setNormalisedAngle(0.14f);  
     enable_motors = 0; 
 
     additional_led=0;
@@ -90,15 +83,8 @@ user_button.fall(&user_button_pressed_fcn);
         additional_led=!additional_led;
         ThisThread::sleep_for(100ms);
     }
-        
-  
-    servo.enable();
-    servo.setNormalisedAngle(80);       //Robi Startposition 90Grad nach unten
-    
-    
 
     while (schleife) {
-
 
         additional_led=1;
         printf("Start\n");
@@ -107,116 +93,113 @@ user_button.fall(&user_button_pressed_fcn);
          switch (zustand) {
 
 
-                case arm_strecken_bis_flach: 
+                case 1: ///////////////////////////////////////////////////////////////////////////////////////
                 printf("Case 1\n");
 
-                fall(80);
-                ThisThread::sleep_for(4000ms);
-                zustand = bis_zum_Hindernis_fahren;
-                break;
+                fall(90.0);
+                ThisThread::sleep_for(5000ms);
 
-                
-                case bis_zum_Hindernis_fahren:
+                zustand ++;
+                break;
+                case 2: ///////////////////////////////////////////////////////////////////////////////////////
                 printf("Case 2\n");
 
                 move(5.2f, 1.0f, 0.5f);
-                ThisThread::sleep_for(4000ms);
+                ThisThread::sleep_for(3000ms);
 
 
-                zustand = arm_heben_bis_hoehe_hindernis;
+                zustand ++;
                 break;
-
-
-
-                case arm_heben_bis_hoehe_hindernis:
+                case 3: ///////////////////////////////////////////////////////////////////////////////////////
                 printf("Case 3\n");
                 
-                lift(100);
-                move(0.5f, 0.6f, 0.5f);
-                ThisThread::sleep_for(6000ms);
+                lift(150.0);
+                ThisThread::sleep_for(3000ms);
                 
-                zustand = fahren_bis_gelenk_ueber_hindernis;
+                zustand ++;
                 break;
-
-                case fahren_bis_gelenk_ueber_hindernis:
+                case 4: ///////////////////////////////////////////////////////////////////////////////////////
                 printf("Case 4\n");
 
 
                 move(4.0f, 0.5f, 0.6f);
-                ThisThread::sleep_for(5000ms);
+                ThisThread::sleep_for(3000ms);
 
-                zustand = arm_strecken_bis_bodenkontakt;
+                zustand ++;
                 break;
-
-
-                case arm_strecken_bis_bodenkontakt:
+                case 5: ///////////////////////////////////////////////////////////////////////////////////////
                 printf("Case 5\n");
 
-                fall(0);
+                fall(70.0);
                 ThisThread::sleep_for(3000ms);
 
                 
-                zustand = fahren_bis_hinderniskante;
+                zustand ++;
                 break;
-
-                move(5.0f, 0.4f, 0.4f);
-                ThisThread::sleep_for(5000ms);
-
-                case fahren_bis_hinderniskante:
+                case 6: ///////////////////////////////////////////////////////////////////////////////////////
                 printf("Case 6\n");
 
+                move(4.0f, 0.4f, 0.6f);
+                fall(20.0);
+                ThisThread::sleep_for(5000ms);
                                      
-                zustand = arm_heben_bis_flach;
+                zustand ++;
                 break;
-
-                lift(80);
-                //ThisThread::sleep_for(5000ms);
-
-                case arm_heben_bis_flach:
+                case 7: ///////////////////////////////////////////////////////////////////////////////////////
                 printf("Case 7\n");
 
+                move(1.0f, 0.4f, 0.7f);
+                lift(90.0);
+                ThisThread::sleep_for(4000ms);
 
-                ThisThread::sleep_for(1000ms);
-                zustand = fahren_bis_ziel;
+
+                zustand ++;
                 break;
-
-                move(5.0f, 1.0f, 0.3f);
-                case fahren_bis_ziel:
+                case 8: ///////////////////////////////////////////////////////////////////////////////////////
                 printf("Case 8\n");
 
+                move(3, 0.5, 0.5);
+                lift(120.0);
+                ThisThread::sleep_for(4000ms);
 
-                ThisThread::sleep_for(1000ms);
-                zustand = arm_heben_in_ausgangsposition;
+
+                zustand ++;
                 break;
-
-
-
-                case arm_heben_in_ausgangsposition:
+                case 9: ///////////////////////////////////////////////////////////////////////////////////////
                 printf("Case 9\n");
 
-                lift(130);
-                ThisThread::sleep_for(1000ms);
-                zustand = arm_strecken_bis_flach;
-                servo.disable();
+                move(4, 0.5, 0.8);
+                fall(90.0);
+                ThisThread::sleep_for(3000ms);
+
+                zustand ++;
                 break;
+                case 10: ///////////////////////////////////////////////////////////////////////////////////////
+                printf("Case 10\n");
+
+                move(3, 0.7, 0.7);
+                ThisThread::sleep_for(3000ms);
 
 
+                zustand ++;
+                break;
+                case 11: ///////////////////////////////////////////////////////////////////////////////////////
+
+                lift(180.0);
+                schleife=false;
+                break;
 
                 default:
                 printf("Default\n");
                 break;
             }
-
-    
-
     }
     
-
 printf("Ende gelende \n");
 }
 
-void user_button_pressed_fcn()
-{
+void user_button_pressed_fcn(){
+
     // do_execute_main_task if the button was pressed
     do_execute_main_task = !do_execute_main_task;
     if (do_execute_main_task) do_reset_all_once = true;
